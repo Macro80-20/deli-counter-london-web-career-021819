73c8372e7b37341bb03e1192752094ad295474c7
fernando <fernando_lopezc@live.com>
2019-02-11 18:12:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,8 @@ linw = ["Amanda","Annette","Ruchi"]
 kats_deli = []
 
 def line (queue)
-  if queue == 0
-    puts"The line is currently empty."
-    end
+  if queue.length == 0
+    puts "The line is currently empty."
 counter = 0
 new_array = []
 queue.each do
