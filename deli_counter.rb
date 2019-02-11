074728a710aaabac997521d2804107f6d226deda
fernando <fernando_lopezc@live.com>
2019-02-11 18:11:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+linw = ["Amanda","Annette","Ruchi"]
+kats_deli = []
+
+linw = ["Amanda","Annette","Ruchi"]
+kats_deli = []
+
+def line (queue)
+  if queue == 0
+    puts"THERE IS NO ONE"
+    end
+counter = 0
+new_array = []
+queue.each do
+|x|
+new_array.push("#{counter+=1}. #{x}")
+end
+ return "The line is currently: #{new_array.join(", ")}"
+end
+
+
+
+#counter = 0
+#queue.each do |x|
+  #counter+=1
+  # puts "The line is currently: #{counter}. #{x}"
+  #end
